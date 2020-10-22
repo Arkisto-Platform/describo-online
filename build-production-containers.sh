2bc72c5e656c41712367ef8d05c176983304022b
Marco La Rosa <m@lr.id.au>
2020-10-22 15:28:52 +1100

    update tag with org name

--- build-production-containers.sh
+++ build-production-containers.sh
@@ -7,21 +7,21 @@ fi
 VERSION="${1}"
 
 echo "Building API container"
-docker build --rm -t describo-online-api:latest -f Dockerfile.api-build .
-docker tag describo-online-api:latest describo-online-api:${VERSION}
+docker build --rm -t utseresearch/describo-online-api:latest -f Dockerfile.api-build .
+docker tag utseresearch/describo-online-api:latest utseresearch/describo-online-api:${VERSION}
 docker rmi $(docker images | grep none | awk '{print $3}')
-exit 
 
 echo "Building UI code"
 docker run --rm -v $PWD/ui:/srv/ui -v $PWD/../describo-ui-plugins:/srv/plugins -w /srv/ui node:14-buster npm run build
 
 echo "Building UI container"
-docker build --rm -t describo-online-ui:latest -f Dockerfile.ui-build .
-docker tag describo-online-ui:latest describo-online-ui:${VERSION}
+docker build --rm -t utseresearch/describo-online-ui:latest -f Dockerfile.ui-build .
+docker tag utseresearch/describo-online-ui:latest utseresearch/describo-online-ui:${VERSION}
 
 docker rmi $(docker images | grep none | awk '{print $3}')
 
 echo "Pushing built containers to docker hub"
+docker login
 docker push utseresearch/describo-online-api:latest
 docker push utseresearch/describo-online-api:${VERSION}
 docker push utseresearch/describo-online-ui:latest
