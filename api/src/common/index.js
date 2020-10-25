50a250cc366ac33c729b9c13184474981c56beda
Marco La Rosa <m@lr.id.au>
2020-10-26 10:28:04 +1100

    add issuer to config when loading

--- api/src/common/index.js
+++ api/src/common/index.js
@@ -2,11 +2,13 @@ import path from "path";
 import { readJSON } from "fs-extra";
 
 export async function loadConfiguration() {
-    const configuration =
+    let configuration =
         process.env.NODE_ENV === "development"
             ? path.resolve(
                   path.join(__dirname, "..", "..", "configuration.json")
               )
             : "/srv/configuration.json";
-    return await readJSON(configuration);
+    configuration = await readJSON(configuration);
+    configuration.ui.services.okta.issuer = `${configuration.ui.services.okta.domain}/oauth2/default`;
+    return configuration;
 }
