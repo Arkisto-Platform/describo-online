a1d8ffa282e71be61a8d9e96d8cf151a804a941f
Marco La Rosa <m@lr.id.au>
2020-11-18 10:48:47 +1100

    wire in render entity component

--- ui/src/components/Shell.component.vue
+++ ui/src/components/Shell.component.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col">
         <navigation-component />
-        <div class="p-2">
+        <div class="p-2 flex flex-col space-y-1">
             <select-target-component />
             <!-- <onedrive-file-preview-component
                 style="width: 500px; height: 500px;"
@@ -12,6 +12,10 @@
                 id="b!-4xxhXFx5kKSQwwAuO7Ek9AvCPQ0yFpGrxxa6HLjh4QGAuTlRxZhQ59yKADIuZ49#01K7QV4XMU2HLRHYXGTVAY4LGNFZOOIF6Z"
             /> -->
             <load-collection-component v-if="target.resource" />
+            <render-entity-component
+                :id="selectedEntityId"
+                v-if="collectionId"
+            />
         </div>
     </div>
 </template>
@@ -20,6 +24,7 @@
 import NavigationComponent from "@/components/Navigation.component.vue";
 import SelectTargetComponent from "@/components/select-target/Shell.component.vue";
 import LoadCollectionComponent from "@/components/LoadCollection.component.vue";
+import RenderEntityComponent from "@/components/ui-components/RenderEntity.component.vue";
 import HTTPService from "./http.service";
 
 export default {
@@ -27,13 +32,20 @@ export default {
         NavigationComponent,
         SelectTargetComponent,
         LoadCollectionComponent,
+        RenderEntityComponent,
     },
     data() {
         return {};
     },
     computed: {
+        collectionId() {
+            return this.$store.state.collection?.id;
+        },
         target() {
             return this.$store.state.target;
+        },
+        selectedEntityId() {
+            return this.$store.state.selectedEntity?.id;
         },
     },
     mounted() {
