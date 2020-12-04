ae96e3b23882428482436704e63f647ebaca09d0
Marco La Rosa <m@lr.id.au>
2020-12-04 11:02:32 +1100

    make icon shown configurable

--- ui/src/components/manage-entities/DeleteProperty.component.vue
+++ ui/src/components/manage-entities/DeleteProperty.component.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="">
         <el-button @click="deleteProperty" type="danger" size="mini">
-            <i class="fas fa-unlink"></i>
+            <div v-show="type === 'unlink'" class="inline-block">
+                <i class="fas fa-unlink"></i>
+            </div>
+            <div v-show="type === 'delete'" class="inline-block">
+                <i class="fas fa-trash"></i>
+            </div>
         </el-button>
     </div>
 </template>
@@ -11,6 +16,13 @@ import DataService from "./data.service.js";
 
 export default {
     props: {
+        type: {
+            type: String,
+            default: "unlink",
+            validator: (val) => {
+                return ["delete", "unlink"].includes(val);
+            },
+        },
         property: {
             type: Object,
             required: true,
