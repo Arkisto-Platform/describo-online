251b150bd4c4bede4276730a3572738129e456c6
Marco La Rosa <m@lr.id.au>
2020-12-08 10:20:46 +1100

    add a definition column so we can track what a property means

--- api/src/models/property.js
+++ api/src/models/property.js
@@ -13,7 +13,9 @@ module.exports = function (sequelize, DataTypes) {
             name: {
                 type: DataTypes.TEXT,
                 allowNull: false,
-                defaultValue: "",
+            },
+            definition: {
+                type: DataTypes.JSON,
             },
             sequenceNumber: {
                 type: DataTypes.INTEGER,
