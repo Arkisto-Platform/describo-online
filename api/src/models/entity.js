38e831ddfda0ca98c4916332803a8997eac21163
Marco La Rosa <m@lr.id.au>
2020-12-08 09:52:22 +1100

    allow entity name to be empty

--- api/src/models/entity.js
+++ api/src/models/entity.js
@@ -19,7 +19,6 @@ module.exports = function (sequelize, DataTypes) {
             },
             name: {
                 type: DataTypes.STRING,
-                allowNull: false,
                 defaultValue: "",
             },
             locked: {
