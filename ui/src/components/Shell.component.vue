f22ce1169e62019f37f294c433d7347d536490e9
Marco La Rosa <m@lr.id.au>
2020-12-08 11:10:16 +1100

    rename tab headings and cleanup

--- ui/src/components/Shell.component.vue
+++ ui/src/components/Shell.component.vue
@@ -3,26 +3,19 @@
         <navigation-component />
         <div class="p-2 flex flex-col space-y-1">
             <select-target-component />
-            <!-- <onedrive-file-preview-component
-                style="width: 500px; height: 500px;"
-                path="/test-rocrate/data/NT1-20003-002.jpg"
-            /> -->
-            <!-- <onedrive-file-preview-component
-                style="width: 500px; height: 500px;"
-                id="b!-4xxhXFx5kKSQwwAuO7Ek9AvCPQ0yFpGrxxa6HLjh4QGAuTlRxZhQ59yKADIuZ49#01K7QV4XMU2HLRHYXGTVAY4LGNFZOOIF6Z"
-            /> -->
+
             <load-collection-component v-if="target.resource" />
             <el-tabs type="border-card" v-model="activeTab" v-if="collectionId && target.resource">
-                <el-tab-pane label="Manage Your Data" name="manageData">
+                <el-tab-pane label="Build the Collection" name="manageData">
                     <render-entity-component
                         :id="selectedEntityId"
                         v-if="activeTab === 'manageData'"
                     />
                 </el-tab-pane>
-                <el-tab-pane label="Manage Content" name="manageContent">
+                <el-tab-pane label="Manage Collection Data Files" name="manageContent">
                     <manage-crate-files-component v-if="activeTab === 'manageContent'" />
                 </el-tab-pane>
-                <el-tab-pane label="Manage Entities" name="manageEntities">
+                <el-tab-pane label="Browse Collection Entities" name="manageEntities">
                     <entity-list-manager-component
                         v-if="activeTab === 'manageEntities'"
                         @manage-data="activeTab = 'manageData'"
