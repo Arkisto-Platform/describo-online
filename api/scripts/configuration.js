fa40a0ff38ca75b7d95eeb27e063dcc8fc7585f0
Marco La Rosa <m@lr.id.au>
2020-12-10 08:42:12 +1100

    change Periodical to Journal

--- api/scripts/configuration.js
+++ api/scripts/configuration.js
@@ -19,6 +19,7 @@ module.exports = {
     rename: {
         classes: {
             MediaObject: "File",
+            Periodical: "Journal",
             "models#Object": "RepositoryObject",
             "models#Collection": "RepositoryCollection",
         },
