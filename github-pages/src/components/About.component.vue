40b7b70130871d407d1be0269964709e60aeba5a
Marco La Rosa <m@lr.id.au>
2021-02-15 12:36:44 +1100

    revise about page text and eu logo

--- github-pages/src/components/About.component.vue
+++ github-pages/src/components/About.component.vue
@@ -2,11 +2,11 @@
     <div class="flex flex-col space-y-4">
         <div class="text-2xl">About</div>
         <div class="p-4">
-            Describo was first developed for the UTS eResearch Team as a proof of concept tool for
-            creating and managing Research Object Crates. The Minimum Viable Product that was
-            created proved to be so effective that it has now formed the basis of the description
-            activities being undertaken by the CS3MESH4EOSC Work Package 4. In addition, it will
-            form a key component of the next generation PARADISEC catalog.
+            Initial Describo development was undertaken by UTS to assist researchers in describing
+            research objects for archiving and publication using the RO-Crate standard. The product
+            that was created proved to be so effective that it has now formed the basis of the
+            description activities being undertaken by the CS3MESH4EOSC Work Package 4. In addition,
+            it will form a key component of the next generation PARADISEC catalog.
         </div>
         <div class="flex flex-col space-y-4">
             <div class="flex flex-row justify-around">
@@ -21,19 +21,21 @@
                     </a>
                 </div>
             </div>
-            <div class="flex flex-row justify-around">
+            <div class="flex flex-col space-y-4 lg:flex-row lg:space-y-0 justify-around">
                 <div class="p-6 bg-black">
                     <a href="https://cs3mesh4eosc.eu/" target="_blank">
-                        <img src="images/logo-cs3mesh4eosc.png" class="h-8" />
+                        <img src="images/logo-cs3mesh4eosc.png" class="xl:h-8" />
                     </a>
                 </div>
                 <div class="p-6 bg-white">
                     <a href="https://www.psnc.pl" target="_blank">
-                        <img src="images/logo-psnc.svg" class="h-8" />
+                        <img src="images/logo-psnc.svg" class="xl:h-8" />
                     </a>
                 </div>
-                <div class="p-4">
-                    <img src="images/logo-eu.jpg" class="h-12" />
+            </div>
+            <div class="flex flex-row justify-around">
+                <div class="p-6">
+                    <img src="images/logo-eu.png" class="xl:h-20" />
                 </div>
             </div>
         </div>
