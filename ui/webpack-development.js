a9dc439acfc7998586c78301f2bee25445980a02
Marco La Rosa <m@lr.id.au>
2021-09-20 09:57:04 +1000

    update app dependencies
    commit 40e144c484889af1191f002b664eff0ff9bc926d
    Author: Marco La Rosa <m@lr.id.au>
    Date:   Mon Sep 20 09:56:47 2021 +1000
    
        update dev server config
    
    commit 9d46559ece58e9bd3d12413d0efae12deb7faf34
    Author: Marco La Rosa <m@lr.id.au>
    Date:   Mon Sep 20 09:56:14 2021 +1000
    
        update app dependencies

--- ui/webpack-development.js
+++ ui/webpack-development.js
@@ -7,12 +7,13 @@ const configuration = merge(common, {
     mode: "development",
     devtool: "eval-source-map",
     devServer: {
-        contentBase: path.join(__dirname, "dist"),
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
         compress: true,
         host: "0.0.0.0",
         port: 9000,
         historyApiFallback: true,
-        writeToDisk: true,
         hot: true,
     },
 });
