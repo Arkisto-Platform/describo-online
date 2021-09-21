0317aa4bd8e158c88773343141204e6494c6ca33
Marco La Rosa <m@lr.id.au>
2021-09-21 14:50:19 +1000

    use site name when no auth configured

--- ui/src/components/Login.component.vue
+++ ui/src/components/Login.component.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col">
         <okta-login-component v-if="login === 'okta'" />
-        <div v-else class="text-center text-xl m-20">Describo Online</div>
+        <div v-else class="text-center text-xl m-20">{{ siteName }}</div>
     </div>
 </template>
 
@@ -10,6 +10,7 @@ export default {
     data() {
         return {
             login: this.$store.state.configuration.login,
+            siteName: this.$store.state.configuration.siteName,
         };
     },
     mounted() {},
