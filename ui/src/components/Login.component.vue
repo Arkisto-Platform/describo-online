6c691e587085c963721d84f6ffb24310a29a27aa
Marco La Rosa <m@lr.id.au>
2022-04-25 17:31:25 +1000

    cleanup

--- ui/src/components/Login.component.vue
+++ ui/src/components/Login.component.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col">
         <okta-login-component v-if="login === 'okta'" />
         <reva-login-component v-else-if="login === 'reva'" />
-        <div v-else class="text-center text-xl m-20">{{ siteName }} mojumbo</div>
+        <div v-else class="text-center text-xl m-20">{{ siteName }}</div>
     </div>
 </template>
 
