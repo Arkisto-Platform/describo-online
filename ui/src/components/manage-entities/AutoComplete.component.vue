a753b901b7955afde4d243635becd17c09991bde
Marco La Rosa <m@lr.id.au>
2022-04-26 10:15:29 +1000

    refactor using composition api

--- ui/src/components/manage-entities/AutoComplete.component.vue
+++ ui/src/components/manage-entities/AutoComplete.component.vue
@@ -12,99 +12,91 @@
         :remote-method="querySearch"
         @change="handleSelect"
     >
-        <el-option-group v-for="group in options" :key="group.label" :label="group.label">
+        <el-option-group v-for="group in data.matches" :key="group.label" :label="group.label">
             <el-option
-                v-for="item in group.options"
+                v-for="item in group.entities"
                 :key="item.id"
                 :label="item.name"
-                :value="item"
-                :value-key="item.id"
+                :value="item.id"
             >
-                <template>
-                    <div class="flex flex-row space-x-2 my-1">
-                        <div class="text-sm">{{ item.etype }}:</div>
-                        <div class="text-sm" v-if="item.name">{{ item.name }}</div>
-                        <div class="text-sm text-right" v-else>{{ item.eid }}</div>
-                    </div>
-                </template>
+                <div class="flex flex-row space-x-2 text-gray-500 text-sm">
+                    <div class="text-sm">{{ item.etype }}:</div>
+                    <div class="text-sm" v-if="item.name">{{ item.name }}</div>
+                    <div class="text-sm text-right" v-else>{{ item.eid }}</div>
+                </div>
             </el-option>
         </el-option-group>
     </el-select>
 </template>
 
-<script>
+<script setup>
 import DataService from "./data.service.js";
+const dataService = new DataService();
+import { ref, reactive, onMounted } from "vue";
 
-export default {
-    props: {
-        type: {
-            type: String,
-            required: true,
+const props = defineProps({
+    type: {
+        type: String,
+        required: true,
+    },
+});
+
+const emit = defineEmits(["link:entity", "add:template", "create:entity"]);
+let selection = ref(undefined);
+const data = reactive({
+    matches: [],
+    entities: [],
+});
+onMounted(() => {
+    querySearch();
+});
+
+async function querySearch(queryString) {
+    selection.value = undefined;
+    data.matches = [];
+    let query = {};
+    let { entities } = await dataService.findEntity({
+        limit: 5,
+        hierarchy: props.type,
+        etype: queryString,
+        eid: queryString,
+        name: queryString,
+    });
+    let { templates, total } = await dataService.getTemplates({
+        type: props.type,
+        filter: queryString,
+        limit: 5,
+    });
+    entities = entities.map((e) => ({ ...e, type: "internal" }));
+    templates = templates.map((e) => ({ ...e, type: "template" }));
+    let matches = [
+        {
+            label: "Create new entity",
+            entities: [],
         },
-    },
-    data() {
-        return {
-            selection: undefined,
-            entities: undefined,
-            options: [],
-        };
-    },
-    mounted() {
-        this.dataService = new DataService();
-        this.querySearch();
-    },
-    methods: {
-        async querySearch(queryString) {
-            this.selection = undefined;
-            this.entities = undefined;
-            let query = {};
-            if (this.by === "id") query.eid = queryString;
-            if (this.by === "name") query.name = queryString;
-            let { entities } = await this.dataService.findEntity({
-                limit: 5,
-                hierarchy: this.type,
-                etype: queryString,
-                eid: queryString,
-                name: queryString,
-            });
-            let { templates, total } = await this.dataService.getTemplates({
-                type: this.type,
-                filter: queryString,
-                limit: 5,
-            });
-            entities = entities.map((e) => ({ ...e, type: "internal" }));
-            templates = templates.map((e) => ({ ...e, type: "template" }));
-            let options = [
-                {
-                    label: "Create new entity",
-                    options: [],
-                },
-                {
-                    label: "Entities in this crate",
-                    options: entities,
-                },
-                {
-                    label: "Saved templates",
-                    options: templates,
-                },
-            ];
-            this.entities = entities;
-            this.templates = templates;
-            this.options = options;
+        {
+            label: "Entities in this crate",
+            entities,
         },
-        handleSelect() {
-            if (this.selection.type === "internal") {
-                let entity = this.entities.filter((e) => e.id === this.selection.id)[0];
-                if (entity) this.$emit("link:entity", { entity });
-            } else if (this.selection.type === "template") {
-                let template = this.templates.filter((t) => t.id === this.selection.id)[0];
-                if (template) this.$emit("add:template", { template });
-            } else {
-                this.$emit("create:entity", { name: this.selection });
-            }
+        {
+            label: "Saved templates",
+            entities: templates,
         },
-    },
-};
+    ];
+    data.matches = matches;
+    data.entities = [...entities, templates];
+}
+function handleSelect(value) {
+    if (value) {
+        const entity = data.entities.filter((e) => e.id === value)[0];
+
+        if (entity.type === "internal") {
+            emit("link:entity", { entity });
+        } else if (entity.type === "template") {
+            emit("add:template", { entity });
+        } else {
+            emit("create:entity", { name: entity });
+        }
+    }
+}
 </script>
-
-<style lang="scss" scoped></style>
