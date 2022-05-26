397d6dfcc159460e4d1f0b67c8ade2564a9e4dff
Marco La Rosa <m@lr.id.au>
2022-05-26 13:16:03 +1000

    formatting; set geojson property with stringified geojson feature

--- ui/src/components/manage-entities/Map.component.vue
+++ ui/src/components/manage-entities/Map.component.vue
@@ -26,37 +26,41 @@ async function init() {
     data.map = new Leaflet.map("map");
     centerMap();
     Leaflet.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}", {
-	attribution:
-	    'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: "abcd",
-	minZoom: 1,
-	maxZoom: 16,
-	ext: "jpg",
-	noWrap: true,
+        attribution:
+            'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        subdomains: "abcd",
+        minZoom: 1,
+        maxZoom: 16,
+        ext: "jpg",
+        noWrap: true,
     }).addTo(data.map);
     data.entity = await dataService.getEntity({ id: props.id });
     data.properties = (
-	await dataService.getEntityProperties({ id: props.id })
+        await dataService.getEntityProperties({ id: props.id })
     ).properties.forwardProperties;
 
     let fg;
     if (data.properties.box) {
-	const boxes = data.properties.box.map((box) => {
-	    box = box.value.split(" ");
-	    return {
-		type: "Polygon",
-		coordinates: [box.map((b) => b.split(","))],
-	    };
-	});
-	removeExistingLayers();
-	fg = addFeatureGroup({ geoJSON: boxes, type: "box" });
-    } else {
-	let geojson = {
-	    type: "Point",
-	    coordinates: [data.properties.longitude[0].value, data.properties.latitude[0].value],
-	};
-	removeExistingLayers();
-	fg = addFeatureGroup({ geoJSON: geojson, type: "point" });
+        const boxes = data.properties.box.map((box) => {
+            box = box.value.split(" ");
+            return {
+                type: "Polygon",
+                coordinates: [box.map((b) => b.split(","))],
+            };
+        });
+        removeExistingLayers();
+        fg = addFeatureGroup({ geoJSON: boxes, type: "box" });
+    } else if (data.properties.latitude && data.properties.longitude) {
+        let geojson = {
+            type: "Point",
+            coordinates: [data.properties.longitude[0].value, data.properties.latitude[0].value],
+        };
+        removeExistingLayers();
+        fg = addFeatureGroup({ geoJSON: geojson, type: "point" });
+    } else if (data.properties.geojson) {
+        let geojson = JSON.parse(data.properties.geojson[0].value);
+        removeExistingLayers();
+        fg = addFeatureGroup({ geoJSON: geojson });
     }
     // await new Promise((resolve) => setTimeout(resolve, 200));
     // data.map.fitBounds(fg.getBounds());
@@ -73,15 +77,15 @@ function removeExistingLayers() {
 function addFeatureGroup({ geoJSON, type }) {
     let fg;
     if (type === "box") {
-	fg = Leaflet.featureGroup([Leaflet.geoJSON(geoJSON, { style: { color: "black" } })]);
+        fg = Leaflet.featureGroup([Leaflet.geoJSON(geoJSON, { style: { color: "black" } })]);
     } else {
-	fg = Leaflet.featureGroup([
-	    Leaflet.geoJSON(geoJSON, {
-		pointToLayer: function (feature, latlng) {
-		    return L.circleMarker(latlng, { color: "black", fillColor: "black" });
-		},
-	    }),
-	]);
+        fg = Leaflet.featureGroup([
+            Leaflet.geoJSON(geoJSON, {
+                pointToLayer: function (feature, latlng) {
+                    return L.circleMarker(latlng, { color: "black", fillColor: "black" });
+                },
+            }),
+        ]);
     }
     fg.addTo(data.map);
     data.layers.push(fg);
