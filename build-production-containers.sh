cfe5f8adca8be4d7403e50cb10adc8486bb41278
Marco La Rosa <m@lr.id.au>
2022-07-06 13:12:38 +1000

    tag and bump version

--- build-production-containers.sh
+++ build-production-containers.sh
@@ -6,33 +6,38 @@ if [ "$#" != 1 ] ; then
 fi
 VERSION="${1}"
 
-read -p '>> Build the code? [y|N] ' resp
+read -p '>> Build the containers? [y|N] ' resp
 if [ "$resp" == "y" ] ; then
-    echo '>> Building the API code'
-    docker build --rm -t arkisto/describo-online-api:latest -f Dockerfile.api-build .
+    echo '>> Building the API container '
+    docker buildx build --platform=linux/amd64,linux/arm64 \
+        --rm \
+        -t arkisto/describo-online-api:latest \
+        -t arkisto/describo-online-api:${VERSION} \
+        -f Dockerfile.api-build .
+    docker buildx build --load \
+        -t arkisto/describo-online-api:latest \
+        -t arkisto/describo-online-api:${VERSION} \
+        -f Dockerfile.api-build .
+
     echo
 
-    echo '>> Building the UI code'
+    echo '>> Building the UI container'
     cd ui
-    # npm run build
     docker run -it --rm \
         -v $PWD:/srv/ui \
         -v $PWD/../../describo-ui-plugins:/srv/ui/src/plugins \
         -v ui_node_modules:/srv/ui/node_modules \
         -w /srv/ui node:14-buster bash -l -c "npm run build"
     cd -
-    echo
-fi
-
-read -p '>> Build the containers? [y|N] ' resp
-if [ "$resp" == "y" ] ; then
-    echo "Building API container"
-    docker tag arkisto/describo-online-api:latest arkisto/describo-online-api:${VERSION}
-
-    echo "Building UI container"
-    docker build --rm -t arkisto/describo-online-ui:latest -f Dockerfile.ui-build .
-    docker tag arkisto/describo-online-ui:latest arkisto/describo-online-ui:${VERSION}
-
+    docker buildx build --platform=linux/amd64,linux/arm64 \
+        --rm \
+        -t arkisto/describo-online-ui:latest \
+        -t arkisto/describo-online-ui:${VERSION} \
+        -f Dockerfile.ui-build .
+    docker buildx build --load \
+        -t arkisto/describo-online-ui:latest \
+        -t arkisto/describo-online-ui:${VERSION} \
+        -f Dockerfile.ui-build .
     echo
 fi
 
@@ -49,12 +54,25 @@ fi
 
 read -p '>> Push the containers to docker hub? [y|N] ' resp
 if [ "$resp" == "y" ] ; then
-    echo "Pushing built containers to docker hub"
     docker login
-    docker push arkisto/describo-online-api:latest
-    docker push arkisto/describo-online-api:${VERSION}
-    docker push arkisto/describo-online-ui:latest
-    docker push arkisto/describo-online-ui:${VERSION}
+
+    echo "Pushing built containers to docker hub"
+    docker buildx build --platform=linux/amd64,linux/arm64 \
+        --push \
+        --rm \
+        -t arkisto/describo-online-api:latest \
+        -t arkisto/describo-online-api:${VERSION} \
+        -f Dockerfile.api-build .
+    docker buildx build --platform=linux/amd64,linux/arm64 \
+        --push \
+        --rm \
+        -t arkisto/describo-online-ui:latest \
+        -t arkisto/describo-online-ui:${VERSION} \
+        -f Dockerfile.ui-build .
+    # docker push arkisto/describo-online-api:latest
+    # docker push arkisto/describo-online-api:${VERSION}
+    # docker push arkisto/describo-online-ui:latest
+    # docker push arkisto/describo-online-ui:${VERSION}
 fi
 
 read -p '>> Remove local container copies? [y|N] ' resp
